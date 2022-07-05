b6c52c79cd0ddf179931ad00dc1308a4a1e695db
Arunachalam B <arun@gogosoon.com>
2022-07-05 10:41:42 +0530

    [Arunachalam] Removes unused import

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-const { ipcRenderer } = window.require("electron");
+// const { ipcRenderer } = window.require("electron");
 
 interface SoftwareProp {
   name: string;
