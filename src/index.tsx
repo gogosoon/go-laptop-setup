f30afbfdbdde77594415f9560585ee93b19c0255
Dhanapandi M <dhanapandi.m@gogosoon.com>
2022-09-08 18:07:06 +0530

    [Davidjayan] Matomo provider added

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createInstance, MatomoProvider } from "@datapunt/matomo-tracker-react";
+import { Constants } from "./Constants/Constants";
+
+const instance = createInstance({
+  urlBase: Constants.MATOMO_URL,
+  siteId: Constants.MATOMO_ZEN_SITE_ID
+});
 
 ReactDOM.render(
   <React.StrictMode>
+    <MatomoProvider  value={instance} >
     <App />
+    </MatomoProvider>,
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
